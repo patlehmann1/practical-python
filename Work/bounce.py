fe389456acc4b6d5a254cb02599eeeed36dedaaa
patlehmann1 <patrick.lehmann@techs-MacBook-Pro-2.local>
2020-07-21 23:05:37 -0400

    added bounce solution

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+bounce_number = 1
+
+while bounce_number <= 10:
+  height = height * .6
+  bounce_number += 1
+  print(bounce_number, round(height, 4))
